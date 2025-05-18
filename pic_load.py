7d96ee4c676aae312d65e64aa0d0b49f476a2ba2
vicktor301211 <iva301211@gmail.com>
2025-05-18 13:40:37 +0300

    Начал делать викторину

---
+++ pic_load.py
@@ -0,0 +1,7 @@
+import pygame
+
+pm_image = pygame.image.load('GFX/PM.png')
+zhukov_image = pygame.image.load('GFX/Zhukov.png')
+o_p_image = pygame.image.load('GFX/orden_pobedy.png')
+suvorov_image = pygame.image.load('GFX/Suvorov.png')
+ak_image = pygame.image.load('GFX/74.png')
